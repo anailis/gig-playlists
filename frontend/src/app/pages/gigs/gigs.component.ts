e95efe43284cca897e345c6f3a78bf9203e3198b
anailis <efummey1079@gmail.com>
2025-05-05 15:57:21 +0100

    group cards by year

--- frontend/src/app/pages/gigs/gigs.component.ts
+++ frontend/src/app/pages/gigs/gigs.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 import {MatToolbar} from "@angular/material/toolbar";
-import {MatIcon} from "@angular/material/icon";
-import {MatIconButton} from "@angular/material/button";
-import {MatCard} from "@angular/material/card";
 import {NgForOf} from "@angular/common";
 import {EventCardComponent} from "../../event-card/event-card.component";
 
@@ -10,6 +7,7 @@ interface Gig {
   day: string;
   date: number;
   month: string;
+  year: number;
   title: string;
 }
 
@@ -17,10 +15,7 @@ interface Gig {
   selector: 'app-gigs',
   standalone: true,
   imports: [
-    MatIcon,
-    MatIconButton,
     MatToolbar,
-    MatCard,
     NgForOf,
     EventCardComponent
   ],
@@ -29,31 +24,50 @@ interface Gig {
 })
 export class GigsComponent {
   gigs: Gig[] = [
-    { day: 'Mon', date: 30, month: 'December', title: 'blink-182 *' },
-    { day: 'Sun', date: 31, month: 'December', title: 'Green Day *' },
-    { day: 'Tue', date: 1, month: 'November', title: 'Everything Everything *' },
-    { day: 'Wed', date: 2, month: 'November', title: 'The World is a Beautiful Place and I Am No Longer Afraid to Die *' },
+    { day: 'Mon', date: 30, month: 'December', year: 2025, title: 'blink-182 *' },
+    { day: 'Sun', date: 31, month: 'December', year: 2025, title: 'Green Day *' },
+    { day: 'Tue', date: 1, month: 'November', year: 2024, title: 'Everything Everything *' },
+    { day: 'Wed', date: 2, month: 'November', year: 2025, title: 'The World is a Beautiful Place and I Am No Longer Afraid to Die *' },
+    { day: 'Wed', date: 2, month: 'April', year: 2025, title: 'Cheekface' },
   ];
 
-  gigsByMonth: { month: string; gigs: Gig[] }[] = [];
+  groupedGigs: {
+    year: number;
+    months: {
+      month: string;
+      gigs: Gig[];
+    }[];
+  }[] = [];
 
   ngOnInit(): void {
-    this.gigsByMonth = this.groupByMonth(this.gigs);
+    this.groupedGigs = this.groupByDate(this.gigs);
   }
 
-  groupByMonth(gigs: Gig[]): { month: string; gigs: Gig[] }[] {
-    const groups: { [month: string]: Gig[]} = {};
+  // TODO: ensure cards can be ordered in both asc and desc order
+  groupByDate(gigs: Gig[]) {
+    const yearMap: { [year: number]: { [month: string]: Gig[] } } = {};
 
     for (const gig of gigs) {
-      if (!groups[gig.month]) {
-        groups[gig.month] = [];
+      if (!yearMap[gig.year]) {
+        yearMap[gig.year] = {};
       }
-      groups[gig.month].push(gig);
+      if (!yearMap[gig.year][gig.month]) {
+        yearMap[gig.year][gig.month] = [];
+      }
+
+      yearMap[gig.year][gig.month].push(gig);
     }
 
-    return Object.keys(groups).map(month => ({
-      month,
-      gigs: groups[month]
-    }));
+    return Object.keys(yearMap)
+        .sort() // sort years ascending
+        .map(year => ({
+          year: +year,
+          months: Object.keys(yearMap[+year])
+              .sort() // sort months alphabetically
+              .map(month => ({
+                month,
+                gigs: yearMap[+year][month]
+              }))
+        }));
   }
 }
