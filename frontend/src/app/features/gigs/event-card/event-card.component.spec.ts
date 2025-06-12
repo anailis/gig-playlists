f9c87ebbc68bba0bf9c82b025d862dd7e99ba696
anailis <efummey1079@gmail.com>
2025-06-12 20:15:20 +0100

    add initial unit test

--- frontend/src/app/features/gigs/event-card/event-card.component.spec.ts
+++ frontend/src/app/features/gigs/event-card/event-card.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EventCardComponent } from './event-card.component';
 
-describe('EventCardComponent', () => {
+fdescribe('EventCardComponent', () => {
   let component: EventCardComponent;
   let fixture: ComponentFixture<EventCardComponent>;
 
@@ -19,5 +19,17 @@ describe('EventCardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render input values correctly', () => {
+    component.day = 'Monday';
+    component.date = 12;
+    component.title = 'Team Meeting';
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('[data-testid="title"]')?.textContent).toContain('Team Meeting');
+    expect(compiled.querySelector('[data-testid="day"]')?.textContent).toContain('Monday');
+    expect(compiled.querySelector('[data-testid="date"]')?.textContent).toContain('12');
   });
 });
