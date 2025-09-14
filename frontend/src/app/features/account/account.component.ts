f98fb071243fb65557d2e2d2b7704ea0113c6715
anailis <efummey1079@gmail.com>
2025-09-14 16:55:29 +0100

    basic formatting of account settings

--- frontend/src/app/features/account/account.component.ts
+++ frontend/src/app/features/account/account.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import {MatAnchor, MatButton, MatIconButton} from "@angular/material/button";
 import {MatIcon} from "@angular/material/icon";
+import {MatCard, MatCardTitle} from "@angular/material/card";
+import {MatDivider} from "@angular/material/divider";
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [
-    MatButton,
-    MatAnchor,
-    MatIconButton,
-    MatIcon
-  ],
+    imports: [
+        MatButton,
+        MatAnchor,
+        MatIconButton,
+        MatIcon,
+        MatCard
+    ],
   templateUrl: './account.component.html',
   styleUrl: './account.component.css'
 })
