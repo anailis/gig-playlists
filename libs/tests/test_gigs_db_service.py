fcb8b8537ee8fc9c058ae9358976214cf0a07d0e
anailis <efummey1079@gmail.com>
2026-01-09 16:49:02 +0000

    tests for get user by id

---
+++ libs/tests/test_gigs_db_service.py
@@ -0,0 +1,29 @@
+from unittest.mock import Mock
+
+import pytest
+from aws_lambda_powertools.event_handler.exceptions import ForbiddenError, NotFoundError
+
+from gigs_db_service import GigsDbService
+
+
+class TestGetUserById:
+    def test_user_cannot_access_other_users_data(self):
+        service = GigsDbService(table=Mock())
+
+        with pytest.raises(ForbiddenError):
+            service.get_user_by_id(user_id="123", requesting_user_id="456")
+
+    def test_404_thrown_when_user_not_found(self):
+        table = Mock()
+        table.query.return_value = {"Count": 0, "Items": []}
+        service = GigsDbService(table=table)
+
+        with pytest.raises(NotFoundError):
+            service.get_user_by_id(user_id="123", requesting_user_id="123")
+
+    def test_user_returned_by_id(self):
+        table = Mock()
+        table.query.return_value = {"Count": 1, "Items": ["user"]}
+        service = GigsDbService(table=table)
+
+        assert service.get_user_by_id(user_id="123", requesting_user_id="123") == "user"
