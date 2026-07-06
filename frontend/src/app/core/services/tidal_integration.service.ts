f1bbb19fc336fea1f6fa4ac7494280d41994c421
anailis <efummey1079@gmail.com>
2026-07-06 17:18:21 +0100

    remove unused vars

--- frontend/src/app/core/services/tidal_integration.service.ts
+++ frontend/src/app/core/services/tidal_integration.service.ts
@@ -6,7 +6,7 @@ import {
     generateRandomCodeVerifier,
     generateRandomState,
 } from "oauth4webapi";
-import {ActivatedRoute, ParamMap} from "@angular/router";
+import {ActivatedRoute} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
 
 @Injectable({
@@ -19,7 +19,6 @@ export class TidalIntegrationService implements IntegrationService {
     private readonly authorizationEndpoint = 'https://login.tidal.com/authorize';
     private readonly tokenEndpoint = 'https://auth.tidal.com/v1/oauth2/token';
     private readonly codeChallengeMethod = 'S256';
-    private readonly integrationType = "TIDAL";
     private STATE_KEY = 'tidal_state';
     private CODE_VERIFIER_KEY = 'tidal_code_verifier';
 
